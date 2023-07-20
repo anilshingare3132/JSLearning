92662f2f17afbf63b342dc17ab7bda81fd4a1395
anilshingare3132 <anilshingare3132@gmail.com>
2023-07-20 22:16:28 +0530

    BasicVariable typeOff function created and committed
    BasicVariable typeOff function created and committed

---
+++ 01_variableAssign.js
@@ -0,0 +1,27 @@
+var myFullName; // Variable Declaration
+console.log(myFullName);
+myFullName = "Anil Shingare"; // Variable Assignment or Initialization
+
+console.log(myFullName);
+myFullName = "Anil Ramchandra Shingare"
+
+console.log(myFullName);
+var myLove;
+console.log(myLove);
+myLove ="I Love only JS"
+console.log(myLove);
+var firstName ="my first name is = anil";
+console.log(firstName);
+var lastName ="my last name is = Shingare";
+console.log(lastName);
+var age ="the age is = 30";
+console.log(age);
+var address ="my address is = Sangola";
+console.log(address);
+var pincode ="the pincode is = 332211";
+console.log(pincode);
+var gender ="Gender is = Male";
+console.log(gender);
+var married = "are you married? = yes ";
+console.log(married);
+
---
+++ 02_typeOf.js
@@ -0,0 +1,41 @@
+var bankName;
+console.log(typeof bankName);
+bankName ="Bank name = SBI Bank";
+console.log(bankName);
+bankName ="Type of variable bank name = String";
+console.log(bankName);
+var mark ="92%";
+console.log(mark);
+mark ="Type of variable mark = String";
+console.log(mark);
+var isWorking ="value of isWorking is = undefined" ;
+console.log(isWorking);
+isWorking ="value of isWorking is =True";
+console.log(isWorking);
+isWorking ="Type of isWorking is = boolean";
+var totalCount ="the total count is = Hundred and Seven";
+console.log(totalCount);
+totalCount ="the type of total count is = String";
+console.log(totalCount);
+totalCount ="the total count is = 107";
+console.log(totalCount);
+totalCount ="the type of variable totalcount is = number";
+var sallary =null
+console.log(typeof sallary);
+console.log('=======================================');
+var salary = null;
+console.log(typeof salary);
+salary = 40000.50; // 
+var typeOfSalary = typeof salary;
+console.log('salary=>', salary, 'typeof salary =>', typeOfSalary);
+salary = "Forty Thousand";
+var salaryTypeOf = typeof salary;
+console.log('salary=>', salary, 'typeof=>', salaryTypeOf);
+var num1 = 100; // 200
+var num2 = 200; // 100
+console.log( 'Before Swap' ,'num1 - ', num1, ' num2 - ', num2);
+var temp = num1;
+num1 = num2;
+num2 = temp;
+console.log(temp, num1, num2);
+console.log( 'After Swap' ,'num1 - ', num1, ' num2 - ', num2);
